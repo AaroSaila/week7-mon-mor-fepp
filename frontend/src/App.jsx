b6894c336a6c2073d087c56521c54a969399227b
oiivantsov <oiivantsov@outlook.com>
2024-12-02 12:05:26 +0200

    frontend part3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 // pages & components
 import Home from "./pages/HomePage";
@@ -12,24 +14,42 @@ import Login from "./pages/Login";
 
 const App = () => {
 
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/add-job" element={<AddJobPage />} />
-              <Route path='*' element={<NotFoundPage />} />
-              <Route path="/jobs/:id" element={<JobPage />} />
-              <Route path="/edit-job/:id" element={<EditJobPage />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-  
-  export default App;
+  const [isAuthenticated, setIsAuthenticated] = useState(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.log(user);
+    return user && user.token ? true : false;
+  });
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/add-job"
+              element={isAuthenticated ? <AddJobPage /> : <Navigate to="/login" />}
+            />
+            <Route path='*' element={<NotFoundPage />} />
+            <Route path="/jobs/:id" element={<JobPage isAuthenticated={isAuthenticated} />} />
+            <Route
+              path="/edit-job/:id"
+              element={isAuthenticated ? <EditJobPage /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/signup"
+              element={isAuthenticated ? <Navigate to="/" /> : <Signup setIsAuthenticated={setIsAuthenticated} />}
+            />
+            <Route
+              path="/login"
+              element={isAuthenticated ? <Navigate to="/" /> : <Login setIsAuthenticated={setIsAuthenticated} />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
